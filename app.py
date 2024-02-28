241ddc0a2de81a848c12d0013c5d2e60a658866d
Leszek Mielnikow <leszekmel@gmail.com>
2024-02-28 10:34:28 +0100

    Update app.py for fastapi

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
-import streamlit as st
-import tools
-from stock_research import Anazlyze_stock
+from fastapi import FastAPI
+from stock_research import main_foo
 
-st.title("Investment Analysis Chatbot")
-st.write("This bot analyses investment opportunities based on the available info from the internet and the power of LLM model")
+app = FastAPI()
 
-query = st.text_input('Input your investment question about a company or sector:') 
+@app.get("/api/getinvestinfo", tags=["investinfo"])
+async def get_invest_info():
+    """
+    Returns a dummy JSON response if the token is valid.
+    """
+    rec = main_foo()
+    
+    return rec
 
-Enter=st.button("Enter")
-clear=st.button("Clear")
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8001)
 
-if clear:
-    print(clear)
-    st.markdown(' ')
-
-if Enter:
-    import time
-    with st.spinner('Analyzing stock prices, financial reports and news'):
-        out=Anazlyze_stock(query)
-    st.success('Done!')
-    st.write(out)
-
-# to run: streamlit run app.py
+# to run: uvicorn app:app --reload
